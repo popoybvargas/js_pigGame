3e8e074865044cdcdb4d4282a39ce8b7d13ed89b
Popoy Vargas <popoyvargas@popoyMBP.local>
2020-08-12 17:14:08 +0800

    Complete game.

--- app.js
+++ app.js
@@ -8,3 +8,79 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+/* VARIABLES */
+const winScore = 50;
+let dice, activePlayer, roundScore, globalScores, totalCurrentScore;
+
+/* DOM ELEMENTS */
+const playerPanel0 = document.querySelector('.player-0-panel');
+const playerPanel1 = document.querySelector('.player-1-panel');
+const globalScoreDOM0 = document.getElementById('score-0');
+const globalScoreDOM1 = document.getElementById('score-1');
+const currentScoreDOM0 = document.getElementById('current-0');
+const currentScoreDOM1 = document.getElementById('current-1');
+const diceImgDOM = document.querySelector('img.dice');
+const rollDiceDOM = document.querySelector('button.btn-roll');
+const holdDOM = document.querySelector('button.btn-hold');
+const newGameDOM = document.querySelector('button.btn-new');
+
+const newGame = () => {
+	activePlayer = 0;
+	roundScore = 0;
+	globalScores = [0, 0];
+	totalCurrentScore = 0;
+	globalScoreDOM0.textContent = 0;
+	globalScoreDOM1.textContent = 0;
+	currentScoreDOM0.textContent = 0;
+	currentScoreDOM1.textContent = 0;
+	diceImgDOM.style.display = 'none';
+	playerPanel0.classList.add('active');
+	playerPanel1.classList.remove('active');
+};
+newGame();
+
+const isPlayer1 = () => activePlayer === 0;
+
+const swithchTurn = () => {
+	playerPanel0.classList.toggle('active');
+	playerPanel1.classList.toggle('active');
+	roundScore = 0;
+	totalCurrentScore = 0;
+	currentScoreDOM0.textContent = 0;
+	currentScoreDOM1.textContent = 0;
+	isPlayer1() ? activePlayer = 1 : activePlayer = 0;
+};
+
+const winCheck = () => {
+	if (totalCurrentScore >= winScore) {
+		alert(`Player ${activePlayer + 1} wins!`);
+		holdScore();
+	}
+};
+
+const rollDice = () => {
+	diceImgDOM.style.display = 'none';
+	dice = Math.ceil(Math.random() * 6);
+	diceImgDOM.src = `dice-${dice}.png`;
+	window.setTimeout(() => {
+		diceImgDOM.style.display = 'block';
+
+		if (dice > 1) {
+			roundScore += dice;
+			isPlayer1() ? currentScoreDOM0.textContent = roundScore : currentScoreDOM1.textContent = roundScore;
+			totalCurrentScore = (document.getElementById(`score-${activePlayer}`).textContent * 1) + roundScore;
+			winCheck();
+		} else { swithchTurn(); }
+	}, 250);
+};
+rollDiceDOM.addEventListener('click', rollDice);
+
+const holdScore = () => {
+	globalScores[activePlayer] = totalCurrentScore;
+	isPlayer1() ? globalScoreDOM0.textContent = totalCurrentScore : globalScoreDOM1.textContent = totalCurrentScore;
+	swithchTurn();
+};
+holdDOM.addEventListener('click', holdScore);
+
+newGameDOM.addEventListener('click', newGame);
